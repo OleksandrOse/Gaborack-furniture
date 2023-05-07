6ad15b8e39ac9b99ce24b820187c1a45efbe3077
Oselskyi Oleksandr <oselskyi3@gmail.com>
2023-05-07 13:20:49 +0300

    Gaborack

--- src/components/ImageBanner/ImageBanner.tsx
+++ src/components/ImageBanner/ImageBanner.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Finger } from 'react-finger';
 import classNames from 'classnames';
 
@@ -17,14 +17,40 @@ export const ImageBanner: FC<Props> = ({
   moveLeft,
   startBanner,
 }) => {
+  const [touchStart, setTouchStart] = useState(0);
+  const [touchEnd, setTouchEnd] = useState(0);
+
+  const minSwipeDistance = 50;
+
+  const onTouchStart = (e: React.TouchEvent<HTMLImageElement>) => {
+    setTouchEnd(0); // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+  
+  const onTouchMove = (e: React.TouchEvent<HTMLImageElement>) => setTouchEnd(e.targetTouches[0].clientX);
+  
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isLeftSwipe = distance > minSwipeDistance;
+    const isRightSwipe = distance < -minSwipeDistance;
+    if (isLeftSwipe) {
+      moveLeft();
+    }
+    if (isRightSwipe) {
+      startBanner();
+    }
+    // add your conditional logic here
+  };
+
   const FingerDiv = Finger('div');
 
   return (
     <FingerDiv
-      onTap={(e) => {
-        moveLeft();
-        console.log(e);
-      }}
+      // onTap={(e) => {
+      //   moveLeft();
+      //   console.log(e);
+      // }}
       onSwipeRight={(e) => {
         console.log(e);
         moveLeft();
@@ -38,6 +64,9 @@ export const ImageBanner: FC<Props> = ({
           'banner__img',
           { 'banner__img--active': activeBanner === index },
         )}
+        onTouchStart={(e) => onTouchStart(e)}
+        onTouchMove={(e) => onTouchMove(e)}
+        onTouchEnd={onTouchEnd}
         // onTouchMove={() => moveLeft()}
       />
     </FingerDiv>
